28e0c4846b3f5b3d8eb51553553994e746508f54
Henrique Baldo Carlos <80064286+boldoboldin@users.noreply.github.com>
2025-02-11 17:50:49 -0300

    Adjustments

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -6,16 +6,14 @@ public class PlayerCtrl : MonoBehaviour
 {
     [SerializeField] DynamicJoystick dynamicJoystick;
 
-    [SerializeField] GameObject player;
     private Rigidbody2D rb;
     private Animator anim;
 
-    private float spd = 0;
     [SerializeField] float maxSpd;
 
     // Start is called before the first frame update
     void Start()
-    {     
+    {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
@@ -28,28 +26,20 @@ public class PlayerCtrl : MonoBehaviour
 
         Vector2 direction = new Vector2(inputX, inputY);
 
-        float currentSpd = Mathf.Lerp(spd, maxSpd, direction.magnitude); //
-
-        if (currentSpd > 0)
-        {
-            anim.SetBool("isWalking", true);
-        }
-        else
-        {
-            anim.SetBool("isWalking", false);
-
-        }
+        float currentSpd = Mathf.Lerp(0, maxSpd, direction.magnitude); //
 
         rb.velocity = direction * currentSpd;
 
+        anim.SetFloat("moveInput", currentSpd);
+
         if (inputX > 0)
         {
-            player.transform.localScale = new (-1,1);
+            transform.localScale = new (-1,1);
         }
 
         if (inputX < 0)
         {
-            player.transform.localScale = new(1,1);
+            transform.localScale = new(1,1);
         }
 
         if (inputY > 0)
