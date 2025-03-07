96e436f8820c70375c951d7cf444f1303110b279
Henrique Baldo Carlos <80064286+boldoboldin@users.noreply.github.com>
2025-03-06 22:55:49 -0300

    NewCombatSystem

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -2,35 +2,42 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    [SerializeField] DynamicJoystick moveJoystick, atkJoystick;
+    [SerializeField] DynamicJoystick leftJoystick, rightJoystick;
 
     private Rigidbody2D rb;
-    private Animator anim;
+    private Animator anim, weaponAnim;
 
-    [SerializeField] protected int hp;
-    [SerializeField] float maxSpd;
+    [SerializeField] private GameObject weapon;
+
+    [SerializeField] private int hp;
+    [SerializeField] float maxSpd, shotTimer;
+    public float currentShotTimer = 0f;
     public int damage;
 
-    private bool canMove = true;
+    private bool isFliped = false, canMove = true;
+
+    public LineRenderer raycastLineRenderer;
+    public LayerMask ignoreLayer;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        weaponAnim = weapon.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Attack();
+        WeaponMove();
         Move();
     }
 
     private void Move()
     {
-        float inputX = moveJoystick.Horizontal;
-        float inputY = moveJoystick.Vertical;
+        float inputX = leftJoystick.Horizontal;
+        float inputY = leftJoystick.Vertical;
 
         Vector2 direction = new Vector2(inputX, inputY);
 
@@ -50,42 +57,103 @@ public class PlayerCtrl : MonoBehaviour
         if (inputX > 0)
         {
             transform.localScale = new(-1, 1);
+            isFliped = true;
         }
 
         if (inputX < 0)
         {
             transform.localScale = new(1, 1);
+            isFliped = false;
         }
     }
 
-    private void Attack()
+    private void WeaponMove()
     {
-        float inputX = atkJoystick.Horizontal;
-        float inputY = atkJoystick.Vertical;
+        float inputX = rightJoystick.Horizontal;
+        float inputY = rightJoystick.Vertical;
 
-        if (inputX != 0 || inputY != 0)
-        {      
-            Vector2 attackDirection = new Vector2(inputX, inputY).normalized;
+        Vector2 direction = new Vector2(-inputX, -inputY);
 
+        if (isFliped)
+        {
+            direction = new Vector2(inputX, inputY);
+        }
+        else
+        {
+            direction = new Vector2(-inputX, -inputY);
+        }
+
+        if (direction.magnitude > 0)
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            angle = Mathf.Clamp(angle, -90f, 90f);
+
+            weapon.transform.rotation = Quaternion.Slerp(weapon.transform.rotation, Quaternion.Euler(0, 0, angle), maxSpd * Time.deltaTime);
+        }
+
+        if (inputX != 0)
+        {
+            if (currentShotTimer <= shotTimer)
+            {
+                currentShotTimer += 1 * Time.deltaTime;
+            }
+            else
+            {
+                Shoot();
+                currentShotTimer = 0f;
+            }
+            
             if (inputX > 0)
             {
                 transform.localScale = new(-1, 1);
+                isFliped = true;
             }
 
             if (inputX < 0)
             {
                 transform.localScale = new(1, 1);
+                isFliped = false;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        {
+            Vector2 direction = -weapon.transform.right;
+
+            RaycastHit2D hit = Physics2D.Raycast(weapon.transform.position, direction, Mathf.Infinity, ~ignoreLayer);
+
+            weaponAnim.SetTrigger("shoot");
+
+            if (isFliped)
+            {
+                direction = weapon.transform.right;
+            }
+            else
+            {
+                direction = -weapon.transform.right;
             }
 
-            if (inputY > 0)
+            if (hit.collider != null)
             {
-                anim.SetTrigger("highAtk");
+                Debug.Log("Hit: " + hit.collider.name);
             }
 
-            if (inputY < 0)
-            {
-                anim.SetTrigger("lowAtk");
-            }
+            //if (raycastLineRenderer != null)
+            //{
+            //    raycastLineRenderer.SetPosition(0, weapon.transform.position);
+
+            //    if (hit.collider != null)
+            //    {
+            //        raycastLineRenderer.SetPosition(1, hit.point);
+            //    }
+            //    else
+            //    {
+            //        raycastLineRenderer.SetPosition(1, weapon.transform.position + (Vector3)direction * 10f);
+            //    }
+            //}
         }
     }
 
